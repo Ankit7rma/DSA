3c8bb646fb8cf3fda40e2c9a51ef6e4a5d4b5650
Ankit Sharma <ankitsharma290101@gmail.com>
2024-04-10 21:51:29 +0530

    Agnostic Binary Search

--- binarysearch.py
+++ binarysearch.py
@@ -1,20 +1,50 @@
-def binarySearch(array, target):
+# def binarySearch(array, target):
+#     n = len(array)
+#     start = 0
+#     end = n - 1
+#     while start<=end:
+#         mid = (start+end)//2
+#         if array[mid]<target:
+#             start = mid+1
+#         elif array[mid]>target:
+#             end = mid-1
+#         else:
+#             return mid
+ 
+
+# Order Agnostic Binary Search 
+def binarySearch(array,target):
     n = len(array)
     start = 0
-    end = n - 1
-    while start <= end:
-        mid = (start + end) // 2
-        if array[mid] > target:
-            end = mid - 1
-        elif array[mid] < target:
-            start = mid + 1
-        else:
+    end = n-1
+
+    isAsc =  array[start]<array[end]
+
+    while start<= end:
+        mid = (start+end)//2
+
+        if array[mid] == target:
             return mid
-    return -1
+        
+        if isAsc:
+            if array[mid]>target:
+                end = mid-1
+            else:
+                start = mid+1
+        else:
+            if array[mid]<target:
+                end = mid-1
+            else:
+                start = mid+1
+        
+
+
+        
+
 
 # Example input
 array = [1, 3, 4, 7, 9]  # The array must be sorted for binary search
-x = 9
+x = 1
 
 # Call the function
 result = binarySearch(array, x)
@@ -24,3 +54,5 @@ if result != -1:
     print(f"Element {x} found at index {result}")
 else:
     print(f"Element {x} not found in the array")
+
+
