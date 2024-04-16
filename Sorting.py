0880c159f4f1de7817411b48bcf35707e07af8f3
Ankit Sharma <ankitsharma290101@gmail.com>
2024-04-16 10:12:15 +0530

    Insertion Sort

--- Sorting.py
+++ Sorting.py
@@ -34,21 +34,45 @@ if __name__ == "__main__":
 import sys 
 A = [64, 25, 12, 22, 11] 
   
+def selectionSort(arr):
 # Traverse through all array elements 
-for i in range(len(A)): 
-      
-    # Find the minimum element in remaining  
-    # unsorted array 
-    min_idx = i 
-    for j in range(i+1, len(A)): 
-        if A[min_idx] > A[j]: 
-            min_idx = j 
-              
-    # Swap the found minimum element with  
-    # the first element         
-    A[i], A[min_idx] = A[min_idx], A[i] 
+    for i in range(len(A)): 
+        
+        # Find the minimum element in remaining  
+        # unsorted array 
+        min_idx = i 
+        for j in range(i+1, len(A)): 
+            if A[min_idx] > A[j]: 
+                min_idx = j 
+                
+        # Swap the found minimum element with  
+        # the first element         
+        A[i], A[min_idx] = A[min_idx], A[i] 
   
 # Driver code to test above 
 print ("Sorted array") 
 for i in range(len(A)): 
     print("%d" %A[i],end=" , ")  
+
+# Insertion Sort 
+def insertionSort(arr):
+    n = len(arr)
+    for i in range(n-1):
+        j = i+1
+        while j>0:
+            if arr[j]<arr[j-1]:
+                arr[j],arr[j-1] = arr[j-1] , arr[j]
+            else:
+                break 
+            j-=1
+
+
+
+# Driver code to test above
+arr = [12, 11, 13, 5, 6]
+insertionSort(arr)
+for i in range(len(arr)):
+    print ("% d" % arr[i])
+
+
+
