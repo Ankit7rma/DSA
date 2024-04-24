5c0edf1913e1f0b7a6d22a1837d539cbdd5c6ed6
Ankit Sharma <ankitsharma290101@gmail.com>
2024-04-24 10:49:44 +0530

    Subset Printing

--- Recursion /Subsets.py
+++ Recursion /Subsets.py
@@ -4,12 +4,16 @@ def subSets(arr,ansString,string):
         arr.append(ansString)
         return  
     ch = string[0]
+    # subSets(arr,ansString,string[1:])
+    # subSets(arr,ansString+ch,string[1:])  this prints ['', 'c', 'b', 'bc', 'a', 'ac', 'ab', 'abc']
     subSets(arr,ansString+ch,string[1:])
-    subSets(arr,ansString,string[1:])
+    subSets(arr,ansString,string[1:])   # this prints ['abc', 'ab', 'ac', 'a', 'bc', 'b', 'c', '']
+
 
 string = "abc" 
 arr=[] 
 ans = subSets(arr,"",string)
 
 sorted_arr = sorted(arr, key=lambda x: len(x))
-print(sorted_arr) 
+print(sorted_arr)  #this prints ['', 'a', 'b', 'c', 'ab', 'ac', 'bc', 'abc'] 
+# print(arr) 
