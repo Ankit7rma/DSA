1abe9571379fece1d521d4dc1a0499dcda8fb0c7
Ankit Sharma <ankitsharma290101@gmail.com>
2024-04-24 08:09:52 +0530

    skip Apple

--- Recursion /Skip a Character.py
+++ Recursion /Skip a Character.py
@@ -1,21 +1,21 @@
 # ----------------CREATING NEW STRING AT EACH CALL
 
-def skipChar(string,k):
+# def skipChar(string,k):
   
-    if len(string)==0:
-        return  ""
-    ch = string[0]
-    if ch == k:
-        return skipChar(string[1:],k)
-    else:
-        return ch + skipChar(string[1:],k)
+#     if len(string)==0:
+#         return  ""
+#     ch = string[0]
+#     if ch == k:
+#         return skipChar(string[1:],k)
+#     else:
+#         return ch + skipChar(string[1:],k)
 
 
-string = "bbacdea"
-k = "a"
+# string = "bbacdea"
+# k = "a"
  
-ans = skipChar( string,k)
-print(ans)
+# ans = skipChar( string,k)
+# print(ans)
 
 
 # --------------PASSING STRING IN CALLS
@@ -34,3 +34,33 @@ print(ans)
 # ans = skipChar(ans,string,k)
 # print(ans)
   
+
+
+# --------------------apple skip
+def skipApple(string):
+  
+    if len(string)==0:
+        return  ""
+    ch = string[0]
+    if string.startswith("apple"):
+        return skipApple(string[5:])
+    else:
+        return ch + skipApple(string[1:])
+
+
+string = "bbapplecdea"
+ 
+ 
+ans = skipApple( string)
+print(ans)
+
+
+
+# ----------Skip app that is not in apple 
+def skipAppNotApple(string):
+
+string = "bbapplecdea"
+ 
+ 
+ans = skipApple( string)
+print(ans)
