4118979280ba0ae678d868e9173cdb4a399dc470
Ankit Sharma <ankitsharma290101@gmail.com>
2024-04-27 20:37:54 +0530

    Number of Permutations

--- Recursion /permutations.py
+++ Recursion /permutations.py
@@ -33,20 +33,36 @@
 
 
 # Array in every loop 
-def permutations(p,up):
-    arr = []
+# def permutations(p,up):
+#     arr = []
+#     if len(up)==0:
+#         arr.append(p)
+#         return arr
+#     ch = up[0]
+#     for i in range(len(p)+1):
+#         first = p[:i]
+#         second = p[i:]
+#         a = permutations(first+ch+second,up[1:])
+#         arr.extend(a)
+
+        
+#     return arr
+
+# ans = permutations("","abc")
+# print(ans)
+
+
+# Print Number of Permutations 
+def permutationsCount(p,up):
+    count = 0
     if len(up)==0:
-        arr.append(p)
-        return arr
+        return 1
     ch = up[0]
     for i in range(len(p)+1):
         first = p[:i]
         second = p[i:]
-        a = permutations(first+ch+second,up[1:])
-        arr.extend(a)
-
-        
-    return arr
-
-ans = permutations("","abc")
+        a = permutationsCount(first+ch+second,up[1:])
+        count+=a
+    return count 
+ans = permutationsCount("","abc")
 print(ans)
