ba2d41bbef042e0b1a3526000e2156d3583bef7f
Ankit Sharma <168418072+Ankit7m16@users.noreply.github.com>
2024-05-01 18:49:03 +0530

    Maze WIth Obstacles

--- backtracking/maze.py
+++ backtracking/maze.py
@@ -39,21 +39,65 @@
 # paths = MazeWaysPrint("", 3, 3)
 # print(paths)
 
-def MazeWaysPrint(p, r, c):
-    paths = []
-    if r == 1 and c == 1:
-        paths.append(p)  # Append the path when reaching the bottom-right corner
-        return paths
-    if r > 1 and c>1:
-        diaPaths = MazeWaysPrint(p + "D", r - 1, c-1)  # Move down
-        paths.extend(diaPaths)
-    if r > 1:
-        down_paths = MazeWaysPrint(p + "V", r - 1, c)  # Move down
-        paths.extend(down_paths)
-    if c > 1:
-        right_paths = MazeWaysPrint(p + "H", r, c - 1)  # Move right
-        paths.extend(right_paths)
-    return paths
+# def MazeWaysPrinta(p, r, c):
+#     paths = []
+#     if r == 1 and c == 1:
+#         paths.append(p)  # Append the path when reaching the bottom-right corner
+#         return paths
+#     if r > 1 and c>1:
+#         diaPaths = MazeWaysPrinta(p + "D", r - 1, c-1)  # Move down
+#         paths.extend(diaPaths)
+#     if r > 1:
+#         down_paths = MazeWaysPrinta(p + "V", r - 1, c)  # Move down
+#         paths.extend(down_paths)
+#     if c > 1:
+#         right_paths = MazeWaysPrinta(p + "H", r, c - 1)  # Move right
+#         paths.extend(right_paths)
+#     return paths
 
-paths = MazeWaysPrint("", 3, 3)
-print(paths)
+# paths = MazeWaysPrinta("", 3, 3)
+# print(paths)
+
+# {With Restriction 
+# def mazeWithRestrictions(p,maze,r,c):
+#     paths = []
+    
+#     if r == len(maze)-1 and c == len(maze[0])-1:
+#         paths.append(p)
+#         return paths
+#     if (maze[r][c]==False):
+#         return paths
+#     if r<len(maze)-1:
+#         dwn = mazeWithRestrictions(p+"D",maze,r+1,c)
+#         paths.extend(dwn)
+#     if c<len(maze[0])-1:
+#         rth = mazeWithRestrictions(p+"R",maze,r,c+1)
+#         paths.extend(rth)
+
+#     return paths
+ 
+# maze = [[True,True,True],[True,False,True],[True,True,True]]
+ 
+# ansArr = mazeWithRestrictions("",maze, 0, 0)
+# print(ansArr)  
+
+
+
+def mazeWithRestrictions(ansArr,p,maze,r,c):
+     
+    if r == len(maze)-1 and c == len(maze[0])-1:
+        ansArr.append(p)
+        return  
+    if (maze[r][c]==False):
+        return  
+    if r<len(maze)-1:
+        mazeWithRestrictions(ansArr,p+"D",maze,r+1,c)
+    if c<len(maze[0])-1:
+        mazeWithRestrictions(ansArr,p+"R",maze,r,c+1)
+
+     
+ansArr=[]
+maze = [[True,True,True],[True,False,True],[True,True,True]]
+mazeWithRestrictions(ansArr,"",maze, 0, 0)
+print(ansArr) 
+# }
