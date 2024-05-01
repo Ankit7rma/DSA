be128a380bbef0a56318338f59ef0f285eec2baa
Ankit Sharma <168418072+Ankit7m16@users.noreply.github.com>
2024-05-01 16:56:52 +0530

    pathsArray

--- backtracking/maze.py
+++ backtracking/maze.py
@@ -23,16 +23,35 @@
 # MazeWaysPrint("", 3, 3)
 
 
+# def MazeWaysPrint(p, r, c):
+#     paths = []
+#     if r == 1 and c == 1:
+#         paths.append(p)  # Append the path when reaching the bottom-right corner
+#         return paths
+#     if r > 1:
+#         down_paths = MazeWaysPrint(p + "D", r - 1, c)  # Move down
+#         paths.extend(down_paths)
+#     if c > 1:
+#         right_paths = MazeWaysPrint(p + "R", r, c - 1)  # Move right
+#         paths.extend(right_paths)
+#     return paths
+
+# paths = MazeWaysPrint("", 3, 3)
+# print(paths)
+
 def MazeWaysPrint(p, r, c):
     paths = []
     if r == 1 and c == 1:
         paths.append(p)  # Append the path when reaching the bottom-right corner
         return paths
+    if r > 1 and c>1:
+        diaPaths = MazeWaysPrint(p + "D", r - 1, c-1)  # Move down
+        paths.extend(diaPaths)
     if r > 1:
-        down_paths = MazeWaysPrint(p + "D", r - 1, c)  # Move down
+        down_paths = MazeWaysPrint(p + "V", r - 1, c)  # Move down
         paths.extend(down_paths)
     if c > 1:
-        right_paths = MazeWaysPrint(p + "R", r, c - 1)  # Move right
+        right_paths = MazeWaysPrint(p + "H", r, c - 1)  # Move right
         paths.extend(right_paths)
     return paths
 
