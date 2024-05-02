9dc3ac0e63ce70635ccbe5b571c5357ca5b50190
Ankit Sharma <168418072+Ankit7m16@users.noreply.github.com>
2024-05-02 08:01:18 +0530

    backtarcking

---
+++ backtracking/MazeInAllDirections.py
@@ -0,0 +1,29 @@
+def mazeInAllDirections(ansArr,p,maze, r, c):
+    
+    if r== len(maze)-1 and c == len(maze[0])-1:
+        ansArr.append(p)
+        return 
+    if(maze[r][c]==False):
+        return 
+    # make changes 
+    maze[r][c]=False
+    if r<len(maze)-1:
+        mazeInAllDirections(ansArr,p+"D",maze,r+1,c)
+    if c<len(maze[0])-1:
+        mazeInAllDirections(ansArr,p+"R",maze,r,c+1)
+    if r>0:
+        mazeInAllDirections(ansArr,p+"U",maze,r-1,c)
+
+    if c>0:
+        mazeInAllDirections(ansArr,p+"L",maze,r,c-1)
+    # Recover all the changes that are made 
+    maze[r][c]= True
+    return ansArr
+
+
+
+     
+ansArr=[]
+maze = [[True,True,True],[True,True,True],[True,True,True]]
+mazeInAllDirections(ansArr,"",maze, 0, 0)
+print(ansArr)  
