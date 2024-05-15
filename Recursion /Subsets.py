6aba470be9b0c450a2d10a1b5e29b8d857fcf0cb
Ankit Sharma <168418072+Ankit7m16@users.noreply.github.com>
2024-05-15 10:33:28 +0530

    Subset iteration

--- Recursion /Subsets.py
+++ Recursion /Subsets.py
@@ -1,37 +1,48 @@
 # # # Return a array of all subsets of the string given 
-# def subSets(arr,ansString,string):
-#     if string=="":
-#         arr.append(ansString)
-#         return  
-#     ch = string[0]
-#     # subSets(arr,ansString,string[1:])
-#     # subSets(arr,ansString+ch,string[1:])  this prints ['', 'c', 'b', 'bc', 'a', 'ac', 'ab', 'abc']
-#     subSets(arr,ansString+ch,string[1:])
-#     subSets(arr,ansString,string[1:])   # this prints ['abc', 'ab', 'ac', 'a', 'bc', 'b', 'c', '']
+def subSets(arr,ansString,string):
+    if string=="":
+        arr.append(ansString)
+        return  
+    ch = string[0]
+    # subSets(arr,ansString,string[1:])
+    # subSets(arr,ansString+ch,string[1:])  this prints ['', 'c', 'b', 'bc', 'a', 'ac', 'ab', 'abc']
+    subSets(arr,ansString+ch,string[1:])
+    subSets(arr,ansString,string[1:])   # this prints ['abc', 'ab', 'ac', 'a', 'bc', 'b', 'c', '']
 
-# string = "abc" 
-# arr=[] 
-# ans = subSets(arr,"",string)
+def subset(arr):
+    outer = [[]]
+    for num in arr:
+        n = len(outer)
+        for i in range(n):
+            internal = outer[i][:]
+            internal.append(num)
+            outer.append(internal)
+    return outer
 
-# sorted_arr = sorted(arr, key=lambda x: len(x))
-# print(sorted_arr)  #this prints ['', 'a', 'b', 'c', 'ab', 'ac', 'bc', 'abc'] 
-# # print(arr) 
+string = "abc" 
+arr=[] 
+ans = subSets(arr,"",string)
+print(arr)
+
+sorted_arr = sorted(arr, key=lambda x: len(x))
+print(sorted_arr)  #this prints ['', 'a', 'b', 'c', 'ab', 'ac', 'bc', 'abc'] 
+# print(arr) 
 
 # Return a array of all subsets of the Array given 
 # Passing the array from outside 
 
-# def subSetArray(arr,ansArrin,ansArr):
-#     if len(arr)==0:
-#         ansArr.append(ansArrin.copy())
-#         return 
-#     item = arr[0]
-#     subSetArray(arr[1:],ansArrin +[item],ansArr)
-#     subSetArray(arr[1:],ansArrin,ansArr)
+def subSetArray(arr,ansArrin,ansArr):
+    if len(arr)==0:
+        ansArr.append(ansArrin.copy())
+        return 
+    item = arr[0]
+    subSetArray(arr[1:],ansArrin +[item],ansArr)
+    subSetArray(arr[1:],ansArrin,ansArr)
     
-# arr = [0,1]
-# ansArr=[]
-# subSetArray(arr,[],ansArr)
-# print(ansArr)
+arr = [1,2]
+ansArr=[]
+subSetArray(arr,[],ansArr)
+print(ansArr)
 
 # Creating the array inside each call 
 # def subSetArray(arr):
@@ -82,26 +93,26 @@
 # test
 
 # Duplicate Elements are there 
-def subsetDuplicate(arr):
-    arr.sort()
-    outer = [[]]
-    start = 0
-    end = 0
-    for i in range(len(arr)):
-        start = 0
-        if (i>0 and arr[i]==arr[i-1]):
-            start = end+1
-        end = len(outer)-1
-        n = len(outer)
-        for j in range(start,n):
-            internal = outer[j][:]
-            internal.append(arr[i])
-            outer.append(internal)
-    return outer
+# def subsetDuplicate(arr):
+#     arr.sort()
+#     outer = [[]]
+#     start = 0
+#     end = 0
+#     for i in range(len(arr)):
+#         start = 0
+#         if (i>0 and arr[i]==arr[i-1]):
+#             start = end+1
+#         end = len(outer)-1
+#         n = len(outer)
+#         for j in range(start,n):
+#             internal = outer[j][:]
+#             internal.append(arr[i])
+#             outer.append(internal)
+#     return outer
 
-arr = [0, 1,2,2]
-ansArr = subsetDuplicate(arr)
-print(ansArr)
+# arr = [0, 1,2,2]
+# ansArr = subsetDuplicate(arr)
+# print(ansArr)
 
 
 
