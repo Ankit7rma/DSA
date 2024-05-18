e8b88cf685d55c06bc190e71e0dd7c7fbd9c8c56
Ankit Sharma <168418072+Ankit7m16@users.noreply.github.com>
2024-05-18 14:48:44 +0530

    Urlify

--- Cracking The Coding Interview/ArraysAndStrings.py
+++ Cracking The Coding Interview/ArraysAndStrings.py
@@ -107,18 +107,60 @@
 }
 
 # URLify
+#  Problem: Write a method to replace all spaces in a string with '%20 You may
+#  * assume that the string has sufficient space at the end to hold the additional
+#  * characters, and that you are given the "true" length of the string. (Note: if
+#  * implementing in Java, please use a character array so that you can perform
+#  * this operation in place.) EXAMPLE Input: "Mr John Smith     ", 13 Output:
+#  * "Mr%20John%20Smith"
 
-def URLify(string,n):
-    s = ""
-    for i in range(n):
-        if string[i]==" ":
-            s+="%20"
-        else:
-            s+=string[i]
+# def URLify(string,n):
+#     s = ""
+#     for i in range(n):
+#         if string[i]==" ":
+#             s+="%20"
+#         else:
+#             s+=string[i]
 
-    return s
-string = "Mr John Smith"
-n = len(string)
-print(n)
-ans = URLify(string,n)
-print(ans)
+#     return s
+# string = "Mr John Smith"
+# n = len(string)
+# print(n)
+# ans = URLify(string,n)
+# print(ans)
+
+
+
+class Urlify:
+    def replace_spaces(self, chars: list, length: int) -> str:
+        if chars is None:
+            return ""
+        
+        space_count = 0
+        # First loop: count spaces within the original length
+        for i in range(length):
+            if chars[i] == ' ':
+                space_count += 1
+        
+        index = length + space_count * 2 - 1
+        # Ensure the list has enough space
+        if index + 1 < len(chars):
+            chars = chars[:index + 1]
+        
+        # Second loop: replace spaces from the end
+        for i in range(length - 1, -1, -1):
+            if chars[i] == ' ':
+                chars[index] = '0'
+                chars[index - 1] = '2'
+                chars[index - 2] = '%'
+                index -= 3
+            else:
+                chars[index] = chars[i]
+                index -= 1
+        
+        return ''.join(chars)
+
+# Example usage
+u = Urlify()
+chars = list("Mr John Smith       ")  # The extra spaces are to accommodate the replacements
+print(u.replace_spaces(chars, 13))  # Output: "Mr%20John%20Smith"
