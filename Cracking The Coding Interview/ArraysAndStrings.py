7c3b69aaecc83a015c42cc7034f0e3fa7c2a0886
Ankit Sharma <168418072+Ankit7m16@users.noreply.github.com>
2024-05-18 08:40:15 +0530

    IsUnique

--- Cracking The Coding Interview/ArraysAndStrings.py
+++ Cracking The Coding Interview/ArraysAndStrings.py
@@ -1,18 +1,14 @@
-def is_unique2(  s)  :
-     
+def is_unique2(s):
     if s is None or len(s) > 26:
         return False
-    
     checker = 0
-    for char in s:
-        val = ord(char) - ord('a')
+    for i in s:
+        val = ord(i) - ord("a")
         if (checker & (1 << val)) > 0:
             return False
-        checker |= (1 << val)
-    
+        checker |= 1 << val
     return True
 
 s = "abcb"
 iu = is_unique2(s)
 print(iu)
-  
